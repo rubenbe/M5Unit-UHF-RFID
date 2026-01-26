0ff85c8e81c662fc65e5d8d0219474099e47da15
Ruben Vandamme <github-com-00ff86@vandamme.email>
2026-01-26 19:55:05 +0100

    Revert "More claude work"
    This reverts commit 76572209f1002ade71171ce96fbcfce32588a6ed.

--- esphome/components/m5rfid/text_sensor/m5rfid_text_sensor.h
+++ esphome/components/m5rfid/text_sensor/m5rfid_text_sensor.h
@@ -3,46 +3,22 @@
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/core/component.h"
 #include "esphome/core/helpers.h"
-#include <map>
-#include <string>
 
 namespace esphome {
 namespace m5rfid {
-
-struct TagInfo {
-  std::string epc;
-  int8_t rssi;
-  uint32_t last_seen;  // millis timestamp
-  uint32_t first_seen; // for burst detection
-};
 
 class M5RFIDTextSensor : public PollingComponent, public text_sensor::TextSensor {
  public:
   M5RFIDTextSensor() : PollingComponent(50 /*ms*/) {}
 
   float get_setup_priority() const override { return esphome::setup_priority::LATE; }
-
+  
   void setup() override;
   void update() override;
   void dump_config() override;
 
-  // Configuration
-  void set_dedup_timeout_ms(uint32_t timeout) { dedup_timeout_ms_ = timeout; }
-  void set_burst_poll_cycles(uint8_t cycles) { burst_poll_cycles_ = cycles; }
-
  protected:
   int scanstate_ = 0;
-
-  // Deduplication: EPC -> TagInfo
-  std::map<std::string, TagInfo> seen_tags_;
-
-  // Configuration
-  uint32_t dedup_timeout_ms_ = 1000;  // Don't report same tag within 1 second
-  uint8_t burst_poll_cycles_ = 5;     // Number of rapid polls per update for burst detection
-
-  // Helpers
-  bool should_report_tag(const std::string &epc, int8_t rssi);
-  void cleanup_old_tags();
 };
 
 }  // namespace m5rfid
